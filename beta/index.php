46c17fa3f3ff92a2d88867fae80517558259998f
tnserver <59087807+tnserver@users.noreply.github.com>
2020-05-28 14:01:53 +0530

    Add files via upload

---
+++ beta/index.php
@@ -0,0 +1,98 @@
+
+
+<!DOCTYPE html><html><head>
+<meta charset="utf-8"/>
+<meta content="IE=edge" http-equiv="X-UA-Compatible"/>
+<meta content="width=device-width,initial-scale=1" name="viewport"/>
+<title>TN Server Extreme Football S01E12 [Toon-Nation]</title>
+<link rel="icon" type="image/png" href=""/>
+<meta name="robots" content="noindex"/>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+<link href="https://fonts.googleapis.com/css?family=Varela+Round&amp;display=swap" rel="stylesheet"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.1/lumen/bootstrap.min.css"/>
+
+<link href="https://cdn.plyr.io/3.5.6/plyr.css" rel="stylesheet"/>
+
+  <!-- If you'd like to support IE8 (for Video.js versions prior to v7) -->
+  <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha256-xaF9RpdtRxzwYMWg4ldJoyPWqyDPCRD0Cv7YEEe6Ie8=" crossorigin="anonymous"></script>
+
+<script src="https://cdn.plyr.io/3.5.6/plyr.js"></script>
+<script>
+      document.addEventListener('DOMContentLoaded', () => {
+          const player = Plyr.setup('.js-player');
+      });
+      
+      
+      
+  </script>
+
+<style>
+body {
+    font-family: 'Varela Round';font-size: 10px;
+}
+/* Style buttons */
+.btn {
+  background-color: DodgerBlue;
+  border: none;
+  color: white;
+  padding: 12px 30px;
+  cursor: pointer;
+  font-size: 18px;
+}
+
+/* Darker background on mouse-over */
+.btn:hover {
+  background-color: RoyalBlue;
+}
+
+
+</style>
+
+
+
+  
+
+		
+<body>
+<section class="newsletter_area">
+<div class="container mt-3">
+<div class="row justify-content-center">
+<div class="container">
+<div class="row">
+<div class="col-lg-12 col-md-12 col-sm-12">
+<header class="w3-display-container w3-content w3-wide" style="max-width:1480px;" id="home">
+  <center><img class="w3-image" src="https://1.bp.blogspot.com/-xi4PHr3Y5to/Xejv8oRWEGI/AAAAAAAAATM/vg1UJo050BQR0XIV7T0EkPmru0kMb96ywCLcBGAsYHQ/s1600/Toon-Nation.jpg" alt="Toon_Nation" width="280" height="auto"/></center>
+ 
+<hr/>
+<center><h4><b>Watch Online</b></h4></center>
+<hr/>
+
+<video class="js-player" poster="">
+
+
+    <source src="https://www.googleapis.com/drive/v3/files/<?=$_GET['NWtBL05lNGhPdnhIWnNLeFN2TG1mZEo1MU93enM1K1luSndtUVdQNlpvclp3WnZqd3NnalhLbEpmUURCSVpWVQ']?>?alt=media&key=AIzaSyBFHimHWDyLOtcNJjA268KwRLhsBuckUxc" type="video/mp4"/>
+
+  </video>
+
+
+   <div>
+   <div>
+   <hr/>
+<center><h5><b>We Do Not Store/Host Any Content on Our Server.</b></h5></center>
+<center><h5><b>All Files are Stored on Google Drive</b></h5></center>
+<hr/>
+<center><h5><b>We do not upload any videos nor do we know who and where videos are coming from. We do not promote any illegal conduct of any kind. Links to the videos are submitted by users and managed by users</b></h5></center>
+<hr/>
+
+
+<center><a href="https://toon-nation.blogspot.com/" target="_blank" rel="noopener"><button class="btn"> Visit Toon-Nation</button></a> </center>
+<hr/>
+
+
+
+
+
+
+</div></div></header></div></div></div></div></div></section></body></html>
