2febfce229a63a0aebe5bc4871b62af6c017e051
tnserver <59087807+tnserver@users.noreply.github.com>
2020-05-30 15:17:06 +0530

    Add files via upload

---
+++ multi/index.php
@@ -0,0 +1,90 @@
+
+
+
+<!DOCTYPE html><html><head>
+<meta charset="utf-8"/>
+<meta content="IE=edge" http-equiv="X-UA-Compatible"/>
+<meta content="width=device-width,initial-scale=1" name="viewport"/>
+<title>Multi Server - TN</title>
+<link rel="icon" type="image/png" href="/img/favicon.png"/>
+<meta name="robots" content="noindex"/>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+<link href="https://fonts.googleapis.com/css?family=Varela+Round&amp;display=swap" rel="stylesheet"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.1/lumen/bootstrap.min.css"/>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha256-xaF9RpdtRxzwYMWg4ldJoyPWqyDPCRD0Cv7YEEe6Ie8=" crossorigin="anonymous"></script>
+
+<style>
+body {
+    font-family: 'Varela Round';font-size: 10px;
+}
+/* Style buttons */
+.btn {
+  background-color: DodgerBlue;
+  border: none;
+  color: white;
+  padding: 12px 30px;
+  cursor: pointer;
+  font-size: 18px;
+}
+
+/* Darker background on mouse-over */
+.btn:hover {
+  background-color: RoyalBlue;
+}
+.resp-container {
+    position: relative;
+    overflow: hidden;
+    padding-top: 56.25%;
+}
+
+.resp-iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: 0;
+}
+
+</style>
+
+
+
+  
+</head>
+<body>
+<section class="newsletter_area">
+<div class="container mt-3">
+<div class="row justify-content-center">
+<div class="container">
+<div class="row">
+<div class="col-lg-12 col-md-12 col-sm-12">
+<header class="w3-display-container w3-content w3-wide" style="max-width:1480px;" id="home">
+  <center><img class="w3-image" src="https://1.bp.blogspot.com/-xi4PHr3Y5to/Xejv8oRWEGI/AAAAAAAAATM/vg1UJo050BQR0XIV7T0EkPmru0kMb96ywCLcBGAsYHQ/s1600/Toon-Nation.jpg" alt="Toon_Nation" width="315" height="auto"/></center>
+  <hr/>
+
+
+<center><h4><b>Watch Online [Multi Server]</b></h4></center>
+<hr/>
+
+  <div class="resp-container"> <iframe class="resp-iframe" src="https://gdrive-player.com/<?=$_GET['id']?>" gesture="media" allow="encrypted-media" allowfullscreen=""></iframe> </div>
+
+
+
+ 
+
+   <div>
+   <div>
+   <hr/>
+<center><h5><b>We Do Not Store/Host Any Content on Our Server.</b></h5></center>
+<center><h5><b>All Files are Stored on Google Drive.</b></h5></center>
+<hr/>
+<center><a href="https://toon-nation.blogspot.com/" target="_blank" rel="noopener"><button class="btn"> Visit Toon-Nation</button></a> </center>
+
+
+
+</div></div></header></div></div></div></div></div></section></body></html>
+
