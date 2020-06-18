21e237b98d5e67dc7ffd7ddf6d0628ca7e7f6589
tnserver <59087807+tnserver@users.noreply.github.com>
2020-06-18 11:19:12 +0530

    Update and rename index.txt to index.php

---
+++ beta/player/index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Beta Server - TN</title>
+	<meta name="robots" content="noindex">
+	<link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon" />
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://ssl.p.jwpcdn.com/player/v/8.8.6/jwplayer.js"></script>
+	<script type="text/javascript">jwplayer.key="cLGMn8T20tGvW+0eXPhq4NNmLB57TrscPjd1IyJF84o=";</script>
+	<style type="text/css" media="screen">html,body{padding:0;margin:0;height:100%}#apicodes-player{width:100%!important;height:100%!important;overflow:hidden;background-color:#000}.apicodes-container{position:relative;width:100%;height:0;padding-bottom:56.25%;background-color:#000;}.apicodes-video{position:absolute;top:0;left:0;width:100%;height:100%;z-index:1;}.apicodes-frame{position:absolute;height:50px;right:8px;margin-top:8px;width:50px;z-index:1;background:url(./assets/images/logo.png);background-repeat:no-repeat;background-size:50px 50px;}</style>
+</head>
+<body>
+
+<div id="apicodes-player"></div><script type="text/javascript">eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('n 2=1s("1-2");2.D({K:[{"q":"A","Z":"g\\/Y","7":"5://W.V.6/c/U/T/e?S=R&Q=P-O"}],N:"16:9",L:"M",B:"C",E:F,G:"H",I:"5://m.1.6/k-j-h-J.10",l:{X:"#12",13:16,1r:0,1q:"1p",1o:"1n"},1m:[{7:"5://m.1.6/k.j.h.1l",q:"1k",11:"l","1j":1i}]});2.1h("./1g/1f/o.1e","1d 1c",f(){n r=1b.1a(2.19()["7"],"18");r.17()},"o");2.p("15",f(){$("#1-2").t("<3 4=\\"1-u\\"> <8 z=\\"5://c.y.6/7/d/e/v\\" w=\\"b%\\" x=\\"b%\\" s=\\"0\\" a=\\"a\\" 4=\\"1-g\\"></8> <3 4=\\"1-i\\"></3></3>")});2.p("14",f(){$("#1-2").t("<3 4=\\"1-u\\"> <8 z=\\"5://c.y.6/7/d/e/v\\" w=\\"b%\\" x=\\"b%\\" s=\\"0\\" a=\\"a\\" 4=\\"1-g\\"></8> <3 4=\\"1-i\\"></3></3>")});',62,91,'|apicodes|player|div|class|https|com|file|iframe||allowfullscreen|100|drive||<?=$_GET['8mM5YuwCygI4oBC9tCcH']?>|function|video|baby|frame|bossg|the|captions|demo|var|download|on|label|win|frameborder|html|container|preview|width|height|google|src|HD|primary|html5|setup|autostart|false|preload|auto|image|poster|sources|startparam|start|aspectratio|urwiL1AUDbv0_KHHSv_JKfJXdrKXk|AIzaSyCP2|key|media|alt|files|v3|googleapis|www|color|mp4|type|jpg|kind|f3f368|fontSize|error|setupError||focus|_blank|getPlaylistItem|open|window|Video|Download|svg|images|assets|addButton|true|default|English|srt|tracks|raised|edgeStyle|Helvetica|fontfamily|backgroundOpacity|jwplayer'.split('|'),0,{}))
+</script>
+</body>
+</html>
